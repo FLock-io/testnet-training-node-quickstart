d3997c4b2aed001d54f1ff52170f0e9c7db4ffcc
srrthk <sarthakkaushal@outlook.com>
2024-10-14 17:13:59 -0400

    Project setup

---
+++ selcted_models.py
@@ -0,0 +1,28 @@
+SUPPORTED_BASE_MODELS = [
+    # qwen1.5
+    "Qwen/Qwen1.5-0.5B",
+    "Qwen/Qwen1.5-0.5B-Chat",
+    "Qwen/Qwen1.5-1.8B",
+    "Qwen/Qwen1.5-1.8B-Chat",
+    "Qwen/Qwen1.5-4B",
+    "Qwen/Qwen1.5-4B-Chat",
+    # qwen2
+    "Qwen/Qwen2-0.5B",
+    "Qwen/Qwen2-0.5B-Instruct",
+    "Qwen/Qwen2-1.5B",
+    "Qwen/Qwen2-1.5B-Instruct",
+    # qwen2.5
+    "Qwen/Qwen2.5-0.5B",
+    "Qwen/Qwen2.5-0.5B-Instruct",
+    "Qwen/Qwen2.5-1.5B",
+    "Qwen/Qwen2.5-1.5B-Instruct",
+    "Qwen/Qwen2.5-3B",
+    "Qwen/Qwen2.5-3B-Instruct",
+    # gemma
+    "google/gemma-2b",
+    # phi3
+    "microsoft/Phi-3.5-mini-instruct",
+    "microsoft/Phi-3-mini-4k-instruct",
+    "microsoft/Phi-3-small-8k-instruct",
+    "microsoft/Phi-3-medium-4k-instruct",
+]
